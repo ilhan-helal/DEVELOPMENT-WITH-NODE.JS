e7ce3c65ab3a3366d3a88d4233edf029a959e9fc
ILHAN HELAL <ilhanhelal30@gmail.com>
2026-02-06 11:50:04 +0530

    Demo node

---
+++ demo.js
@@ -0,0 +1,22 @@
+// console.log("Start");
+
+// console.log("Doing work");
+
+// setTimeout(() => {
+//   console.log("setTimeout executed");
+// }, 1000);
+
+// console.log("3. Sync: End");
+
+
+// const  num1 = Number(process.argv[2]); 
+// const  num2 = Number(process.argv[3]);
+// const sum = num1 + num2;
+// console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+// const http = require('http');
+// http.createServer((req, res) => {
+//   res.write('ILHAN HELAL');
+//   res.end();
+// }).listen(3000, () => {
+//   console.log('Server running at http://localhost:3000/');});
